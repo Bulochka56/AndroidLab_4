f502e5adacb74b73ad20a899237f212807d6d5ba
Булат Хайруллин <kagariakko74@gmail.com>
2023-11-09 18:04:16 +0300

    Add text size change

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
+    private lateinit var button14: Button
+    private lateinit var button16: Button
+    private lateinit var button18: Button
+    private lateinit var button20: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.textView)
+        button14 = findViewById(R.id.button14sp)
+        button16 = findViewById(R.id.button16sp)
+        button18 = findViewById(R.id.button18sp)
+        button20 = findViewById(R.id.button20sp)
+        val size = 14
+
+        button14.setOnClickListener(View.OnClickListener {
+            text.setTextSize(14f)
+        })
+        button16.setOnClickListener(View.OnClickListener {
+            text.setTextSize(16f)
+        })
+        button18.setOnClickListener(View.OnClickListener {
+            text.setTextSize(18f)
+        })
+        button20.setOnClickListener(View.OnClickListener {
+            text.setTextSize(20f)
+
+        })
     }
 }
